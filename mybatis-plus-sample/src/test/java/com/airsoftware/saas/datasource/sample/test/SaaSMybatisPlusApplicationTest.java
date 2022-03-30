10d3f8cf9d891613a0f7ddb87baa94bf964f2aa5
airoland <airoland@qq.com>
2022-03-30 19:29:10 +0800

    1.4.0

--- mybatis-plus-sample/src/test/java/com/airsoftware/saas/datasource/sample/test/SaaSMybatisPlusApplicationTest.java
+++ mybatis-plus-sample/src/test/java/com/airsoftware/saas/datasource/sample/test/SaaSMybatisPlusApplicationTest.java
@@ -1,12 +1,12 @@
 package com.airsoftware.saas.datasource.sample.test;
 
 import com.airsoftware.saas.datasource.sample.service.UserService;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
@@ -16,8 +16,8 @@ import javax.servlet.http.HttpSession;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(SpringRunner.class)
 @AutoConfigureMockMvc
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class SaaSMybatisPlusApplicationTest {
 
